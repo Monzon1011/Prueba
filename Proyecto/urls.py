aee806339594610b9884541e73d1eecf869b1158
Martin Monzon <callefalsan2017@gmail.com>
2019-09-19 14:35:31 -0300

    Corrigiendo errores 1

--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^Producto/', views.listarProductos)),
+    url(r'^Producto/', views.listarProductos),
     url(r'^Personas/', views.listarPersonas),
     url(r'^Compras/', views.listarCompra),
 )
