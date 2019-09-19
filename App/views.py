44329cfebc270d983fb10cee5dc4b661cea0c0a6
Martin Monzon <callefalsan2017@gmail.com>
2019-09-19 14:11:29 -0300

    Tercer commit - Creacion de los Template

--- App/views.py
+++ App/views.py
@@ -8,11 +8,11 @@ from models import Compra
 
 def listarProductos(request):
     p = Producto.objects.all()
-    return render_to_response('producto.html', { 'productos' : p })
+    return render_to_response('Producto.html', { 'productos' : p })
 
 def listarPersonas(request):
     per = Persona.objects.all()
-    return render_to_response('persona.html', { 'personas' : per })
+    return render_to_response('Personas.html', { 'personas' : per })
 
 def listarCompra(request):
     if request.GET.get('id'):
@@ -20,4 +20,4 @@ def listarCompra(request):
         c = Compra.objects.filter(id=idCompra)
     else:
         c = Compra.objects.all()
-        return render_to_response('compra.html', { 'compras' : c })
+        return render_to_response('Compra.html', { 'compras' : c })
