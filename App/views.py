9a5614c9a4f8554e9ab28823d2ff9463071f62c7
Martin Monzon <callefalsan2017@gmail.com>
2019-09-19 13:52:17 -0300

    Segundo commit - Creacion de las views

--- App/views.py
+++ App/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+
+from models import Persona
+from models import Producto
+from models import Compra
 
 # Create your views here.
+
+def listarProductos(request):
+    p = Producto.objects.all()
+    return render_to_response('producto.html', { 'productos' : p })
+
+def listarPersonas(request):
+    per = Persona.objects.all()
+    return render_to_response('persona.html', { 'personas' : per })
+
+def listarCompra(request):
+    if request.GET.get('id'):
+        idCompra = request.GET.get('id')
+        c = Compra.objects.filter(id=idCompra)
+    else:
+        c = Compra.objects.all()
+        return render_to_response('compra.html', { 'compras' : c })
