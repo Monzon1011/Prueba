25328c575a0b190cc5f651af5f8b417b685d99cf
Martin Monzon <callefalsan2017@gmail.com>
2019-09-19 14:28:04 -0300

    Commit Doble - admin + urls

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from models import Persona
+from models import Producto
+from models import Compra
+
 # Register your models here.
+
+admin.site.Register(Producto)
+
+admin.site.Register(Persona)
+
+admin.site.Register(Compra)
--- Proyecto/urls.py
+++ Proyecto/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from App import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,4 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^Producto/', views.listarProductos)),
+    url(r'^Personas/', views.listarPersonas),
+    url(r'^Compras/', views.listarCompra),
 )
